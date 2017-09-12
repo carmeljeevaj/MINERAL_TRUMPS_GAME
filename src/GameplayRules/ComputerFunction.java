6c2d5b3d477ad57a64996ec5ad035c5fbc97a95d
carmeljeevaj <carmeljeevaj6@gmail.com>
2017-09-12 18:28:48 +0800

    ComputerFunction class written

--- src/GameplayRules/ComputerFunction.java
+++ src/GameplayRules/ComputerFunction.java
@@ -1,4 +1,48 @@
 package GameplayRules;
 
+import Cards.Card;
+import Cards.PlayCard;
+import Cards.TrumpCard;
+
+import java.util.Random;
+
 public class ComputerFunction {
+    public Card chooseCard(Card currentCard, String currentCat, Players.Player currentPlayer) {
+        if (currentCard == null){
+            for (Card card : currentPlayer.hand){
+                if (card instanceof PlayCard){
+                    return card;
+                }
+            }
+        }
+        for (Card card : currentPlayer.hand) {
+            if (card instanceof PlayCard && currentCard instanceof PlayCard){
+                if(card.CompareValue((PlayCard) currentCard, currentCat)){
+                    return card;
+                }
+
+            }
+            else
+            if (currentCard instanceof TrumpCard && card instanceof PlayCard){
+                return card;
+            }
+
+        }
+        for (Card card : currentPlayer.hand){
+            if (card instanceof TrumpCard){
+                return card;
+            }
+        }
+        if(currentCard == null){
+            System.out.println(currentPlayer.hand.size());
+            throw new NullPointerException("Current card is null :(");
+        }
+        return currentCard;
+    }
+
+    public String choose_Category(String categories){
+        String[] select_Categories =  categories.split(", ");
+        return select_Categories[new Random().nextInt(select_Categories.length)];
+    }
+
 }
