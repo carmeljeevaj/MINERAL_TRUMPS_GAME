aae701e5247e1290f8bf524ab3cb1fdfa4a514ad
carmeljeevaj <carmeljeevaj6@gmail.com>
2017-09-30 12:58:29 +0800

    HumanHandler Modified

--- src/Players/HumanHandler.java
+++ src/Players/HumanHandler.java
@@ -2,8 +2,9 @@ package Players;
 import Cards.Card;
 import Cards.PlayCard;
 import Cards.TrumpCard;
+import GUI.GameFunction;
+
 import java.util.InputMismatchException;
-import java.util.Scanner;
 
 /*
 
@@ -11,9 +12,17 @@ Mineral SuperTrumps Card Game by 13422239 (CARMELJEEVA JOHNJEYASEELAN);
 
  */
 public class HumanHandler {
+    private boolean wasInputEntered = false;
+    private String categoryWasSelected = "";
+    private int cardWasSelected;
+    public static HumanHandler humanHandler;
+
+    public HumanHandler(){
+        humanHandler = this;
+    }
 
     public String getCategory(String categories) {
-        System.out.println("\n Choose a Category to play: \n");
+        //System.out.println("\n Choose a Category to play: \n");
         String[] tempCategorys = categories.split(", ");
         //handles if there is only 1 category passed in.
         if (tempCategorys.length == 1) {
@@ -22,22 +31,25 @@ public class HumanHandler {
         for (int i = 0; i < tempCategorys.length; i++) {
             System.out.println(i + " : " + tempCategorys[i]);
         }
-        int userInput = -1;
-        while (userInput < 0 || userInput > tempCategorys.length - 1) {
+        //int userInput = -1;
+        GameFunction.gameFunction.displayCatChoice();
+        while (!wasInputEntered){
             try {
-                System.out.println("\nEnter Your choice: ");
-                userInput = new Scanner(System.in).nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("Please Enter Valid Number Input");
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("There is no Category at: " + userInput);
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                }
             }
-        }
-        return tempCategorys[userInput];
+            wasInputEntered = false;
+            GameFunction.gameFunction.hideCatChoice();
+        return categoryWasSelected;
+        //return localCats[userInput];
     }
 
-    public Card getCard(Card currentCard, String currentCategory, Players.Player currentPlayer) {
+    public Card getCard(Card currentCard, String currentCategory, Player currentPlayer) {
+        GameFunction.gameFunction.handGUIGenerator(currentPlayer.hand);
         System.out.println("\nChoose A Card: \n");
+
         //handles if there is only 1 category passed in.
         for (int i = 0; i < currentPlayer.hand.size(); i++) {
             System.out.println(i + " : " + currentPlayer.hand.get(i).toString());
@@ -47,17 +59,29 @@ public class HumanHandler {
         while (userInput < 0 || userInput > currentPlayer.hand.size()) {
             try {
                 System.out.println("Enter Your choice: ");
-                userInput = new Scanner(System.in).nextInt();
-
-                if (userInput == currentPlayer.hand.size()) {
-                    return currentCard;
+                //userInput = new Scanner(System.in).nextInt();
+                while (!wasInputEntered) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
-            } catch (InputMismatchException e) {
+                    userInput = cardWasSelected;
+                    wasInputEntered = false;
+
+                    if (userInput == currentPlayer.hand.size()) {
+                        return currentCard;
+                    }
+
+
+            }catch (InputMismatchException e) {
                 System.out.println("Please Enter Valid Number Input");
             }
             if (userInput > 0 && userInput < currentPlayer.hand.size()) {
                 if (currentCard instanceof PlayCard && !currentPlayer.hand.get(userInput).CompareValue((PlayCard) currentCard, currentCategory)) {
                     System.out.println(currentPlayer.hand.get(userInput).title + " is not higher than " + currentCard.title);
+                    GameFunction.gameFunction.changeStatus("Card You Selected, Is not better than the current card.");
                     userInput = -1;
                 } else if (currentCard instanceof TrumpCard) {
                     return currentPlayer.hand.get(userInput);
@@ -68,5 +92,14 @@ public class HumanHandler {
         }
 
         return currentPlayer.hand.get(userInput);// returns the hand with userinput
+
+}
+    public void categoryNotifier(String category){
+        categoryWasSelected = category;
+        wasInputEntered = true;
+    }
+    public void cardNotifier(int selectedCard){
+        cardWasSelected = selectedCard;
+        wasInputEntered = true;
     }
 }
