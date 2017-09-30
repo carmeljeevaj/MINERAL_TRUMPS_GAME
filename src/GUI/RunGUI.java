0f79fe80dc2e4a3582adf45a3120e5e7a71340cf
carmeljeevaj <carmeljeevaj6@gmail.com>
2017-09-30 12:59:31 +0800

    Main for Run created

---
+++ src/GUI/RunGUI.java
@@ -0,0 +1,15 @@
+package GUI;
+/*
+
+Mineral SuperTrumps Card Game by 13422239 (CARMELJEEVA JOHNJEYASEELAN);
+
+ */
+public class RunGUI {
+    public static void main(String[] args) {
+        CardGameGUI();
+    }
+    public static void CardGameGUI(){
+        new MenuSource("Mineral Super Trumps Card Game");
+
+    }
+}
