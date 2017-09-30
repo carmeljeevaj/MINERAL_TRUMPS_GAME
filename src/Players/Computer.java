355affc238b37d6f8c85206665e582fb551c900c
carmeljeevaj <carmeljeevaj6@gmail.com>
2017-09-30 12:57:27 +0800

    Computer Class modified

--- src/Players/Computer.java
+++ src/Players/Computer.java
@@ -5,7 +5,7 @@ package Players;
 Mineral SuperTrumps Card Game by 13422239 (CARMELJEEVA JOHNJEYASEELAN);
 
  */
-public class Computer extends Players.Player {
+public class Computer extends Player {
     public PlayerType getPlayerType() {
         return PlayerType.COMPUTER;
     }
